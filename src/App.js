59193be28ab0f2cb7307529646e49d54afe96abe
Abhinesh <your.email@example.com>
2024-05-11 13:19:12 +0530

    added failed to fetch data

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
       const data = await res.json();
       setData(data);
     } catch (e) {
-      alert("Data not Fetching");
+      alert("failed to fetch data");
     }
   }
 
